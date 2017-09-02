13112fb764f345ce9960cc7a752da9a77bc4953a
Unknown <andreymendozaf@gmail.com>
2017-09-01 23:34:10 -0700

    Repaso de C e inicio de Sockets

---
+++ Server/headers/server.h
@@ -0,0 +1,13 @@
+#ifndef SERVER_SERVER_H
+#define SERVER_SERVER_H
+
+#include <sys/socket.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int run(void);
+
+
+
+#endif //SERVER_SERVER_H
---
+++ Server/source/server.c
@@ -0,0 +1,8 @@
+#include "../headers/server.h"
+
+int run(void)
+{
+
+   return 0;
+}
+
