311ced15e8d84e7a260726fcd2a9b2f73a12c82b
Armando López C <armando12101996@gmail.com>
2017-09-17 16:41:13 -0600

    Metiendo Basura al Rep

--- Server/cmake-build-debug/cmake_install.cmake
+++ Server/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/mackygero/Documents/WebServerSchemas/Server
+# Install script for directory: /home/armando/Documentos/S.O/WebServerSchemas/Server
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/mackygero/Documents/WebServerSchemas/Server/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/armando/Documentos/S.O/WebServerSchemas/Server/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
