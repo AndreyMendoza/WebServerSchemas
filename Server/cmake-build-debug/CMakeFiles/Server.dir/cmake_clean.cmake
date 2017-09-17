eca9bcaf18fab289aa03c98e4e01207b2b467922
Unknown <andreymendozaf@gmail.com>
2017-09-17 11:02:00 -0700

    Ignorando ignores

---
+++ Server/cmake-build-debug/CMakeFiles/Server.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Server.dir/main.c.o"
+  "CMakeFiles/Server.dir/source/server.c.o"
+  "CMakeFiles/Server.dir/source/List.c.o"
+  "Server.pdb"
+  "Server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
